de9cbc818772c151cc760e0d867f25a7c339177c
Daniel Boyé <boyelanrewaju@gmail.com>
2023-01-07 00:40:50 +0100

    Update

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-
-
-def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
-    from magic_calculation_102 import add, sub
-
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return (c)
-
-    else:
-        return(sub(a, b))
+if __name__ == "__main__":
+    from variable_load_5 import a
+    print("{:d}".format(a))
