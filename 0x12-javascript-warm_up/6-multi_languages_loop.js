e334225a9605c83bfec39175e98142a0e440a853
Daniel Boye <boyelanrewaju@gmail.com>
2023-04-11 12:29:41 -0700

    changes

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-const myArray = [
-  'C is fun',
-  'Python is cool',
-  'Javascript is amazing'
-];
-for (let i = 0; i < myArray.length; i++) {
-  console.log(myArray[i]);
+const strings = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i = 0; i < strings.length; i++) {
+	  console.log(strings[i]);
 }
